342c744e867113646ba9ce299ac3875152921230
Nathan.B <N.Briscoe1@gmail.com>
2015-12-15 22:32:41 -0600

    Update Perprograming2.js

--- Perprograming2.js
+++ Perprograming2.js
@@ -8,21 +8,84 @@ $(document).ready(function(){
 	$('#container').append("<button class = 'button-click'>Create</button>");
 	$('#container').on('click','.button-click',buttonClick);
 	$('#container').on('click','.remove',buttonRemove);
-
+	$('#button').on('click', buttonReload);
 	
-function buttonClick() {
 
-	$('#container').append('<div> <p> </p> </div>');
+	for (var i = 0; i < cohortArray.length;i++) {
+		$('#container').append('<div id=' + i + '> <p>' + cohortArray[i] + '</p> </div>');
+		$('#container').children().last().hide();
+		$('#container').children().last().append("<button class = 'remove'> Remove </button>");
+	    
+	}
+
+
+
+function buttonClick() {
+	$('#container').append('<div> <p>' + cohortArray[cohortArray.length-1] + '</p> </div>');
 	$('#container').children().last().append("<button class = 'remove'> Remove </button>");
-    $('#container').append(cohortArray[cohortArray.length-1]);
     revealed.push(cohortArray.pop());
     
+	
+}
+//Why is it that We have two buttonClick's but when I try to combine them it doesn't work properly?
+var counter = 0;
+
+function buttonClick() {
+	$("#" + counter).slideDown(); 
+	counter++;
 }
 
 function buttonRemove (){
-	$(this).parent().remove();
+	
+	$(this).parent().fadeOut();
 }
 
+function buttonReload(){
+	$('#button').click(function(){
+		location.reload();
+	});
+}
 
 });
 
+
+
+
+
+
+
+// function buttonClick() {
+
+// 	$('#container').append("<div> <p> </p> </div>");
+// 	$('#container').children().last().append("<button class = 'remove'> Remove </button>");
+//     $('#container').children().last().children().first().append(cohortArray[cohortArray.length-1]);
+//     revealed.append(cohortArray.pop());
+    
+// }
+
+
+
+// function buttonClick() {
+// 	//take the last student in cohort Array, embed it in a paragraph and a div
+// 	//append the whole thing to container div 
+// 	$('#container').append('<div> <p>' + cohortArray[cohortArray.length-1] + '</p> </div>');
+// 	//append a Remove Button to our new div, under the paragraph with the student name
+// 	$('#container').children().last().append("<button class = 'remove'> Remove </button>");
+// 	//$('#container')
+// 	//remove that student from cohort array and push to revealed array
+//     revealed.push(cohortArray.pop());
+    
+//}
+
+//original code before robby's new addition. new code may have fault in it over creating undefined classmates
+// $('#container').append('<p>' + theta[clickCounter] + '<button class="button">Remove</button></p>');
+// 		$('#container').children().last().hide().slideDown();
+// 		clickCounter++;
+
+
+
+
+
+
+
+// });
